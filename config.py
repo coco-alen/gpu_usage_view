5aa086bfef128283c5e47de68cfcfeb6183d4a33
KOKOMI12345 <3072252442@qq.com>
2025-04-05 13:20:48 +0800

    优化代码结构，做了些美化工作，同时优化了SSH交互部分

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 
-# choose in ['cn', 'en']
-# 'cn' for Chinese, 'en' for English
-language = 'cn'
+# choose in ["zh_CN", "en_US"]
+# zh_CN for Chinese, en_US for English
+language = "zh_CN"
 
 page_update_freq = 1 # update page every 1 seconds  
+
+log_level = "INFO" # choose in ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
